fef8cb582281ce82e1609e0e73b58d22026c5704
Alexandre Donisete <ale.bezerra.donisete@gmail.com>
2025-05-12 17:55:38 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/alexandredonisete/workshopmongo/resources/UserResource.java
+++ src/main/java/com/alexandredonisete/workshopmongo/resources/UserResource.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
