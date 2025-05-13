f1fc7f4ec87e9b2e0a0ab24ea4e6b3eea9e3fcfb
Alexandre Donisete <ale.bezerra.donisete@gmail.com>
2025-05-13 17:25:49 -0300

    Consulta simples com @Query

--- src/main/java/com/alexandredonisete/workshopmongo/resources/util/URL.java
+++ src/main/java/com/alexandredonisete/workshopmongo/resources/util/URL.java
@@ -4,9 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 public class URL {
-
     public static String decodeParam(String text) {
-
         try {
             return URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
